a7b586454ff24894817b24c9324baec3a6c6082f
Asmitha <asmithapuppala@gmail.com>
2026-05-01 22:27:13 +0530

    Delete test.py

--- test.py
+++
@@ -1,8 +0,0 @@
-import os
-from dotenv import load_dotenv, find_dotenv
-
-# This will find the .env file automatically in your project
-load_dotenv(find_dotenv())
-
-print("API Key:", os.getenv("MY_API_KEY"))
-
